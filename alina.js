08a59a16fcb08f91b168e9458fbe4f88c4408e96
ajasniew <alina.jasniewski@stonybrook.edu>
2015-08-26 14:52:07 -0400

    Test commits

--- alina.js
+++ alina.js
@@ -1,4 +1,4 @@
-console.log('jonas.js loaded :-) and today is Aug 26!')
+console.log('alina.js loaded :-) and today is Aug 26!')
 document.body.innerHTML+=' <h2>hello from the console Alina</h2>'
 console.log('hello world')
 lala = function(x){
