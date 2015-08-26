08503dedc156ddcaf5bc691b575588c4370ca720
Javi <javi@hscnet-vlan1941-172-31-73-119.uhmc.sunysb.edu>
2015-08-26 11:54:05 -0400

    for fun

--- jonas.js
+++ jonas.js
@@ -2,8 +2,7 @@ console.log('jonas.js loaded :-) and today is Aug 26!')
 document.body.innerHTML+=' <h2>hello from the console</h2>'
 console.log('hello world')
 lala = function(x){
-    var lele=9
-    4
+    var lele=90
     
 }
 ////
