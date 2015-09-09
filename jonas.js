483e7f6bed2b7e506312394bac071f8fbb0d7df3
Jonas Almeida <jalmeida@mathbiol.org>
2015-09-09 09:21:33 -0400

    playing w XMLHttpRequest

--- jonas.js
+++ jonas.js
@@ -1,9 +1,24 @@
-console.log('jonas.js loaded :-) and today is Aug 26!')
-document.body.innerHTML+=' <h2>hello from the console</h2>'
-console.log('hello world')
+console.log('jonas.js loaded :-O and today is Aug 26!')
+document.body.innerHTML+=' <h2>hello from the console :O</h2>'
+console.log('hello world!')
 lala = function(x){
     var lele=9
     4
     
 }
+
+
+function callback () {
+  console.log(this.responseText);
+}
+
+url = 'https://health.data.ny.gov/resource/5q8c-d6xq.json?patient_zipcode=11790'
+
+xhr = new XMLHttpRequest();
+xhr.addEventListener("load", callback);
+xhr.open("GET", url, true);
+xhr.send();
+
+//
+
 ////
