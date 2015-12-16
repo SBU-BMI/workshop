be3e6ce484db07d7926cb9f35adb9700f08303db
Jonas Almeida <jalmeida@mathbiol.org>
2015-12-16 11:04:36 -0500

    ebolaJonas update: plot of death rates working :-)

--- ebolaJonas/ebolaJonas.js
+++ ebolaJonas/ebolaJonas.js
@@ -26,18 +26,54 @@ ebolaJonas.cleanData=function(x){ // recognize types
         return xi
     })
     console.log('clean data:',x)
-    ebolaJonas.plotData(x)
+    // transpose data
+    var parms=Object.getOwnPropertyNames(x[0])
+    var y ={}
+    parms.forEach(function(p){
+    	y[p]=[]
+    })
+    x.forEach(function(xi,i){ // discuss i, why use it here?
+    	parms.forEach(function(p){
+			y[p][i]=xi[p]
+		})
+    })
+    console.log('transposed data:',x)
+    
+    ebolaJonas.plotData(y)
 }
 
 ebolaJonas.plotData=function(x){
     // create DOM element for plot.ly
+    
     var div = document.createElement('div') // using primitives for ol'times sake
     div.id = 'plotlyDiv'
     document.body.appendChild(div)
+    
+    /*  // basic plot for comparison
     Plotly.plot( div, [{
 	x: [1, 2, 3, 4, 5],
 	y: [1, 2, 4, 8, 16] }], {
 	margin: { t: 0 } } );
+	*/
+
+	//see https://plot.ly/javascript/line-and-scatter/ for plot.ly data structure
+	var data = []
+	var parms=Object.getOwnPropertyNames(x)
+	parms.forEach(function(p){
+		if(p!='date'){
+			var trace = {
+			  x: x.date,
+			  y: x[p],
+			  mode: 'markers',
+			  type: 'scatter',
+			  name: p
+			};
+			data.push(trace)
+		}
+	})
+
+	Plotly.newPlot(div, data);
+
 }
 
 
