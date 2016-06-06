279fe43ed85424160c064ae3d65540481291a80d
ajasniew <alina.jasniewski@stonybrook.edu>
2016-06-06 14:12:30 -0400

    Test commit

--- alina.js
+++ alina.js
@@ -1,6 +1,6 @@
 console.log('alina.js loaded :-) and today is Aug 26!')
 document.body.innerHTML+=' <h2>hello from the console Alina</h2>'
-console.log('hello world')
+console.log('hello world 2')
 lala = function(x){
     var lele=9
     4
